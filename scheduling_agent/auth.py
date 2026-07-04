bf063b9dbf70945fca6ede9441c041d6a1909bd5
doron aviv <doronaviv002@gmail.com>
2026-07-04 21:36:00 +0000

    feat: add auth support

--- scheduling_agent/auth.py
+++ scheduling_agent/auth.py
@@ -9,3 +9,9 @@ from googleapiclient.discovery import build
 
 
 SCOPES = [
+    "https://www.googleapis.com/auth/gmail.modify",
+    "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/calendar",
+]
+
+
