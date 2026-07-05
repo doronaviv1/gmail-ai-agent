2ce3469eafd5a15d600203398a2f2307573b28b6
doron aviv <doronaviv002@gmail.com>
2026-07-05 03:12:00 +0000

    test: cover test scheduler behavior

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,3 +1,9 @@
 from datetime import datetime, time
 from zoneinfo import ZoneInfo
 
+from scheduling_agent.models import MeetingRequest, SchedulingAction, TimeSlot
+from scheduling_agent.scheduler import Scheduler
+
+
+class FakeCalendar:
+    def __init__(self, busy: list[TimeSlot] | None = None):
