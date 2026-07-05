a04a8324edcd53104d6a0c1f8d88cdbf86a6ae26
doron aviv <doronaviv002@gmail.com>
2026-07-05 02:15:00 +0000

    test: add test agent coverage

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -5,3 +5,9 @@ from scheduling_agent.agent import SchedulingAgent
 from scheduling_agent.config import Config
 from scheduling_agent.models import EmailMessage, MeetingRequest, SchedulingAction, TimeSlot
 from scheduling_agent.scheduler import Scheduler
+
+
+class FakeGmail:
+    def __init__(self):
+        self.marked = []
+        self.replies = []
