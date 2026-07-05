3d3ca0446da16c97fe73bff7bdf7b0b8b16ee413
doron aviv <doronaviv002@gmail.com>
2026-07-05 01:36:00 +0000

    feat: add scheduler support

--- scheduling_agent/scheduler.py
+++ scheduling_agent/scheduler.py
@@ -8,3 +8,9 @@ from .calendar_client import CalendarClient
 from .models import MeetingRequest, SchedulingAction, SchedulingDecision, TimeSlot
 
 
+@dataclass
+class Scheduler:
+    calendar: CalendarClient
+    timezone: ZoneInfo
+    workday_start: time
+    workday_end: time
