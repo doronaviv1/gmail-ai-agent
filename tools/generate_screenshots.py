701854525f6843d0a5aed63079cb323e6788721b
doron aviv <doronaviv002@gmail.com>
2026-07-05 03:42:00 +0000

    chore: add screenshot tooling

--- tools/generate_screenshots.py
+++ tools/generate_screenshots.py
@@ -17,3 +17,10 @@ def write_png(path: Path, width: int, height: int, accent: tuple[int, int, int])
             if 24 < x < width - 24 and 24 < y < height - 24:
                 base = (246, 248, 250)
             else:
+                base = (36, 41, 47)
+            if 70 < x < width - 70 and 80 < y < 125:
+                base = accent
+            if 70 < x < width - 70 and 155 < y < 175:
+                base = (210, 215, 222)
+            if 70 < x < width - 220 and 210 < y < 235:
+                base = (121, 184, 255)
