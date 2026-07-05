08d4c62f19c96588ac41c393b787720f8a057a96
doron aviv <doronaviv002@gmail.com>
2026-07-05 01:33:00 +0000

    feat: wire scheduler logic

--- scheduling_agent/scheduler.py
+++ scheduling_agent/scheduler.py
@@ -2,3 +2,9 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from datetime import date, datetime, time, timedelta
+from zoneinfo import ZoneInfo
+
+from .calendar_client import CalendarClient
+from .models import MeetingRequest, SchedulingAction, SchedulingDecision, TimeSlot
+
+
