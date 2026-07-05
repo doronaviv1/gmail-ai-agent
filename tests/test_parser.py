8e4f2404209ff37ce79033ac2bb7936c4fb26668
doron aviv <doronaviv002@gmail.com>
2026-07-05 02:57:00 +0000

    test: refine test parser cases

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,3 +3,9 @@ from zoneinfo import ZoneInfo
 
 from scheduling_agent.llm_parser import LLMMeetingParser
 from scheduling_agent.models import EmailMessage
+
+
+def test_fallback_parser_extracts_tomorrow_and_time():
+    parser = LLMMeetingParser(api_key=None, model="unused", timezone=ZoneInfo("UTC"))
+    email = EmailMessage(
+        id="1",
